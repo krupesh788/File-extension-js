4b5993fcbf790e37c812b33281be79d989fe7565
Krupesh Khalas <71176180+krupesh788@users.noreply.github.com>
2024-01-04 14:22:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+function fileextension(){
+
+
+            let f1 = document.getElementById("f1").value;
+            
+            extension = f1.split('.').pop();
+
+            
+            document.getElementById("p1").innerHTML = `File Extension: ${extension}`;
+
+
+
+            // window.location.pathname = f1;
+
+}
